6507645e217cdbcb8ff015408b7a7d2677a3dc84
Lucy <lucy@example.com>
2026-04-17 06:20:17 +0200

    nix-settings: fix indentation

--- nix-settings.nix
+++ nix-settings.nix
@@ -1,18 +1,21 @@
 { lib, config, ... }:
 
 {
-  nix = {
+nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
       trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWyzPY0oqSzmC7kiNqdCXUcrKHC6A="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "ollama.cachix.org-1:5Sgl02t2AMtL/y8rfl7GeG5p+rM6UXCdN2nJZh3BDk="
+        "cache.nixos-cuda.org-1:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
       substituters = [
         "https://cache.nixos.org"
+        "https://ollama.cachix.org"
+        "https://cache.nixos-cuda.org"
       ];
     };
-    warnings = "";
   };
 
   nixpkgs.config.allowUnfree = true;
