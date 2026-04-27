6b84a0ba7986efdd637e9e3fce3fd62a4b8e10cb
Lucy <lucy@example.com>
2026-04-27 10:18:27 +0200

    refactor(home): generate repeated config from nix data

--- home/lucy/programs/alacritty/default.nix
+++ home/lucy/programs/alacritty/default.nix
@@ -1,86 +1,50 @@
-# NOT AFFECTED by Lassulus/wrappers PR #135
-# This wrapper uses pkgs.stdenvNoCC.mkDerivation directly, not the wrappers library.
-{ pkgs ? import <nixpkgs> { } }:
+{ config, lib, ... }:
 
-let
-  alacrittyConfig = ''
-    font:
-      normal:
-        family: "Hack"
-        style: Regular
-      bold:
-        family: "Hack"
-        style: Bold
-      italic:
-        family: "Hack"
-        style: Italic
-      size: 18
+{
+  options.lucy.alacritty = {
+    enable = lib.mkEnableOption "Alacritty configuration";
+  };
 
-    colors:
-      primary:
-        background: '#282828'
-        foreground: '#ebdbb2'
-      cursor:
-        text: '#282828'
-        cursor: '#ebdbb2'
-      normal:
-        black:   '#282828'
-        red:     '#cc241d'
-        green:   '#98971a'
-        yellow:  '#d79921'
-        blue:    '#458588'
-        magenta: '#b16286'
-        cyan:    '#689d6a'
-        white:   '#a89984'
-      bright:
-        black:   '#928374'
-        red:     '#fb4934'
-        green:   '#b8bb26'
-        yellow:  '#fabd2f'
-        blue:    '#83a598'
-        magenta: '#d3869b'
-        cyan:    '#8ec07c'
-        white:   '#ebdbb2'
+  config = lib.mkIf config.lucy.alacritty.enable {
+    programs.alacritty = {
+      enable = true;
+      settings = lib.mkForce {
+        font = {
+          normal = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Italic";
+          };
+          size = 15;
+        };
 
-    window:
-      opacity: 0.8
-      padding:
-        x: 10
-        y: 10
-      dynamic_padding: true
+        window = {
+          opacity = 0.8;
+          blur = true;
+          padding = {
+            x = 10;
+            y = 10;
+          };
+          dynamic_padding = true;
+        };
 
-    scrolling:
-      history: 10000
+        scrolling.history = 10000;
 
-    cursor:
-      style:
-        shape: Block
-        blinking: On
-      unfocused_hollow: true
-  '';
-in
-
-pkgs.stdenvNoCC.mkDerivation {
-  pname = "alacritty-with-config";
-  version = "0.1.0";
-
-  src = pkgs.alacritty;
-
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    mkdir -p $out/etc/xdg/alacritty
-    
-    cp $src/bin/alacritty $out/bin/alacritty
-    cp -r $src/lib $out/lib
-    cp -r $src/share $out/share
-    
-    echo '${alacrittyConfig}' > $out/etc/xdg/alacritty/alacritty.yml
-    
-    wrapProgram $out/bin/alacritty \
-      --set XDG_CONFIG_HOME "$out/etc/xdl"
-  '';
-
-  outputs = [ "out" ];
+        cursor = {
+          style = {
+            shape = "Block";
+            blinking = "On";
+          };
+          unfocused_hollow = true;
+        };
+      };
+    };
+  };
 }
--- home/lucy/programs/packages.nix
+++ home/lucy/programs/packages.nix
@@ -1,20 +1,38 @@
 { config, pkgs, lib, ... }:
 
-{
-  options.lucy.programs = {
-    jetbrains-mono = lib.mkEnableOption "JetBrains Mono font";
-    nautilus = lib.mkEnableOption "Nautilus file manager";
-    comma = lib.mkEnableOption "comma (run programs without installing)";
-    android-studio = lib.mkEnableOption "Android Studio";
-    fuzzel = lib.mkEnableOption "fuzzel (app launcher)";
+let
+  packageOptions = {
+    jetbrains-mono = {
+      description = "JetBrains Mono font";
+      package = pkgs.jetbrains-mono;
+    };
+    nautilus = {
+      description = "Nautilus file manager";
+      package = pkgs.nautilus;
+    };
+    comma = {
+      description = "comma (run programs without installing)";
+      package = pkgs.comma;
+    };
+    android-studio = {
+      description = "Android Studio";
+      package = pkgs.android-studio;
+    };
+    fuzzel = {
+      description = "fuzzel (app launcher)";
+      package = pkgs.fuzzel;
+    };
   };
+in
+{
+  options.lucy.programs = lib.mapAttrs (_: value: lib.mkEnableOption value.description) packageOptions;
 
   config = {
-    home.packages = lib.optionals config.lucy.programs.jetbrains-mono [ pkgs.jetbrains-mono ]
-      ++ lib.optionals config.lucy.programs.comma [ pkgs.comma ]
-      ++ lib.optionals config.lucy.programs.android-studio [ pkgs.android-studio ]
-      ++ lib.optionals config.lucy.programs.fuzzel [ pkgs.fuzzel ]
-      ++ lib.optionals config.lucy.programs.nautilus [ pkgs.nautilus ];
+    home.packages = lib.concatMap
+      (name:
+        lib.optionals config.lucy.programs.${name} [ packageOptions.${name}.package ]
+      )
+      (lib.attrNames packageOptions);
 
     home.sessionVariables = {
       WALLPAPER = "/home/lucy/Pictures/s-l1600.jpg";
