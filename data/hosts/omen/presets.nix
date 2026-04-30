c0511574872e35af1029e35c02e08fa56f3b23ae
Lucy <lucy@example.com>
2026-04-30 12:27:16 +0200

    refactor(host): split omen data by concern

---
+++ data/hosts/omen/presets.nix
@@ -0,0 +1,4 @@
+[
+  "gaming-base"
+  "gaming-steam"
+]
---
+++ data/hosts/omen/settings.nix
@@ -0,0 +1,28 @@
+{
+  lucy.base.sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAT5LcBzQCMfPyq0t29vGjz6UCcTXKZWROmUy82A0lrS";
+  lucy.base.sshKeyComment = "lucy@p50";
+
+  networking.hostName = "omen";
+  networking.networkmanager.enable = true;
+
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.availableKernelModules = ["r8169"];
+  boot.kernelParams = [
+    "tpm.disable=1"
+    "nvme_core.default_ps_max_latency_us=0"
+    "console=tty1"
+  ];
+
+  nix.settings.require-sigs = false;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+  nix.optimise.automatic = true;
+
+  security.run0-sudo-shim.enable = true;
+  services.openssh.settings.PermitRootLogin = "prohibit-password";
+
+  system.stateVersion = "25.11";
+}
