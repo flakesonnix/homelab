2be4bd3374bfce8036054db5e6fb08e16fb0d201
Lucy <lucy@example.com>
2026-04-30 12:55:48 +0200

    refactor(host): move omen overrides into host data

--- hosts/omen/host.nix
+++ hosts/omen/host.nix
@@ -6,29 +6,16 @@
 }: let
   projectLib = import ../../lib;
   hostData = import ../../data/hosts/omen {
-    inherit lib pkgs;
-  };
-
-  hyfetch-wrapped = wrappers.lib.wrapPackage {
-    inherit pkgs;
-    package = pkgs.hyfetch;
-    flags = {
-      "-p" = "transgender";
-    };
+    inherit lib pkgs wrappers;
   };
 in {
-  config =
-    projectLib.framework.host.applyHost {
-      inherit lib;
-      host = hostData;
-      presetRoot = ../../data/presets;
-      packagePath = ["lucy"];
-      basePackagePath = ["lucy" "basePackages"];
-      fontPackagePath = ["fonts" "packages"];
-    }
-    // {
-      hardware.nvidia.powerManagement.enable = lib.mkForce false;
-
-      environment.systemPackages = [hyfetch-wrapped];
-    };
+  config = projectLib.framework.host.applyHost {
+    inherit lib;
+    host = hostData;
+    presetRoot = ../../data/presets;
+    packagePath = ["lucy"];
+    basePackagePath = ["lucy" "basePackages"];
+    systemPackagePath = ["environment" "systemPackages"];
+    fontPackagePath = ["fonts" "packages"];
+  };
 }
