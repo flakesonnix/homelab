f595caafb341f627285b87afac1314e789c96ecf
Lucy <lucy@example.com>
2026-04-30 12:45:01 +0200

    refactor(secrets): source asterisk creds from sops

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -6,6 +6,11 @@
   options = {
     lucy.secrets = {
       enable = lib.mkEnableOption "sops-nix secrets management";
+      defaultSopsFile = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        default = null;
+        description = "Default sops file for host secrets.";
+      };
       ageKeyPath = lib.mkOption {
         type = lib.types.path;
         default = /etc/sops/age/keys.txt;
@@ -15,19 +20,22 @@
   };
 
   config = lib.mkIf config.lucy.secrets.enable {
-    sops = {
-      defaultSopsFile = ./secrets.yaml;
-      age.keyFile = config.lucy.secrets.ageKeyPath;
-      secrets = {
-        pppoe-password = {
-          sopsFile = ./secrets.yaml;
-          format = "binary";
+    sops =
+      {
+        age.keyFile = config.lucy.secrets.ageKeyPath;
+        secrets = {
+          pppoe-password = {
+            sopsFile = ./secrets.yaml;
+            format = "binary";
+          };
+          luks-key = {
+            sopsFile = ./secrets.yaml;
+            format = "binary";
+          };
         };
-        luks-key = {
-          sopsFile = ./secrets.yaml;
-          format = "binary";
-        };
+      }
+      // lib.optionalAttrs (config.lucy.secrets.defaultSopsFile != null) {
+        inherit (config.lucy.secrets) defaultSopsFile;
       };
-    };
   };
 }
